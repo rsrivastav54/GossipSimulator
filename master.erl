2328f3c659090e0f632b7b5f2e0ac8a5053b6f18
rsrivastav54 <70634373+rsrivastav54@users.noreply.github.com>
2022-10-08 14:12:56 -0400

    Made changes for gossip algo

--- master.erl
+++ master.erl
@@ -14,7 +14,7 @@ start()->
     {ok, NodeCount} = io:read("Number of nodes to start: "),
     {ok, Topology} = io:read("Give a topology from the below options: \n 1: Full Networked \n 2: 2D Grid \n 3: Line \n 4: Imperfect 3D Grid \n"),
     {ok, Algorithm} = io:read("Algorthm from the options: \n 1: Push-Sum \n 2: Gossip \n"),
-
+    Master = self(),
     register(master, self()),
 
     if
@@ -29,42 +29,58 @@ start()->
     % when all the nodes are spawned, start one of the nodes
     % then wait for nodes to ask for the Pids for one of the neighbors
     % spawn(server, loop, [maps:new()]).
-    spawn_nodes(0, NewNodeCount, maps:new(), Topology, Algorithm, NewNodeCount),
-    unregister(master),
-    exit(self(), ok).
+    spawn_nodes(0, NewNodeCount, maps:new(), Topology, Algorithm, NewNodeCount, Master).
+    %unregister(master),
+    %exit(self(), ok).
 
-spawn_nodes(_, NodeCount, Map, _, Algorithm, 0) ->
+spawn_nodes(_, NodeCount, Map, _, Algorithm, 0, _) ->
     StartIndex = (rand:uniform(NodeCount) - 1), % 0 to Nodecount -1
     {ok, StartPid} = maps:find(StartIndex, Map),
+    MapCount = maps:put(StartIndex, StartPid, maps:new()),
+    io:fwrite("Map : ~p\n",[Map]),
     io:fwrite(" The first message to index ~p and Pid ~p\n", [StartIndex, StartPid]),
     if (Algorithm == 1) ->
         StartPid ! {0, 1}; % may be make it asynchornous
     true -> %Algorithm == 2
         StartPid ! {"Maa Chudha"}
     end,
-    get_neighbor_pid(Map, 0, NodeCount);
+    get_neighbor_pid(Map, MapCount, 2, NodeCount);
 
-spawn_nodes(Index, NodeCount, Map, Topology, Algorithm, FinalCount) ->
+spawn_nodes(Index, NodeCount, Map, Topology, Algorithm, FinalCount, Master) ->
     if (Algorithm == 1) ->
-        Pid = spawn(push_sum_node, start, [Index, NodeCount, Topology]);
+        Pid = spawn(push_sum_node, start, [Index, NodeCount, Topology, Master]);
     true -> % Algorithm == 2
-        Pid = spawn(gossip_node, start, [Index, NodeCount, Topology])
+        Pid = spawn(gossip_node, start, [Index, NodeCount, Topology, Master])
     end,
     UpdatedMap = maps:put(Index, Pid, Map),
-    spawn_nodes(Index+1, NodeCount, UpdatedMap, Topology, Algorithm, FinalCount-1).
+    spawn_nodes(Index+1, NodeCount, UpdatedMap, Topology, Algorithm, FinalCount-1, Master).
 
-get_neighbor_pid(_, NodeCount, NodeCount) ->
-    io:fwrite("Shuting the master ~n");
+get_neighbor_pid(_, CountMap, NodeCount, NodeCount) ->
+    io:fwrite("Final Communicated Map ::: ~p\n Finish Count : ~p Node Count : ~p\n",[CountMap, NodeCount, NodeCount]),
+    io:fwrite("Convergence Achieved, Shuting the master ~n"),
+    unregister(master),
+    exit(self(), ok);
+    %ok;
 
-get_neighbor_pid(Map, FinishCount, NodeCount) ->
+get_neighbor_pid(Map, CountMap, FinishCount, NodeCount) ->
     receive
         {SenderPid, Index} ->
             {ok, NeighborPid} = maps:find(Index, Map),
+            A = maps:find(Index, CountMap),
+            SenderPid ! {NeighborPid},
+            if (A =:= {ok,NeighborPid})->
+                get_neighbor_pid(Map, CountMap, FinishCount, NodeCount);
+            true ->
+                UpdatedMap = maps:put(Index, NeighborPid, CountMap),
+                %SenderPid ! {NeighborPid},
+                io:fwrite("Communicated Map ::: ~p\n Finish Count : ~p Node Count : ~p\n",[UpdatedMap, FinishCount, NodeCount]),
+                get_neighbor_pid(Map, UpdatedMap, FinishCount+1, NodeCount)
+            end    
             % io:fwrite("Master: Neighbor of ~p is ~p\n", [SenderPid, NeighborPid]),
             %maybe update the list of nodes who have started sending
-            SenderPid ! {NeighborPid},
-            get_neighbor_pid(Map, FinishCount, NodeCount);
-        {SenderIndex} ->
-            io:fwrite("~p converged, finishcount: ~p\n", [SenderIndex, FinishCount+1]),
-            get_neighbor_pid(Map, FinishCount+1, NodeCount)
+            %SenderPid ! {NeighborPid},
+            %get_neighbor_pid(Map, CountMap, FinishCount, NodeCount);
+        % {SenderIndex} ->
+        %     io:fwrite("~p converged, finishcount: ~p\n", [SenderIndex, FinishCount+1]),
+        %     get_neighbor_pid(Map, CountMap, FinishCount+1, NodeCount)
     end.
