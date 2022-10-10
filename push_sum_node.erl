bf16688dbbc61073942a057995705cbf99515db5
Yash Ranjan <yashranjan@ufl.edu>
2022-10-10 00:17:57 -0400

    Push Sum Code.
    Signed-off-by: Yash Ranjan <yashranjan@ufl.edu>

--- push_sum_node.erl
+++ push_sum_node.erl
@@ -1,21 +1,100 @@
 -module(push_sum_node).
--export([start/3]).
+-export([start/4]).
+-export([send_neighbor/8]).
+-import(neighbor, [get_neighbor_list/4]).
 
-cur_state(Cursum, Curweight, Index, NodeCount, Topology) ->
+
+send_neighbor(_, NodeCount, _, _, _, ParentPid, _, NodeCount) ->
+    ParentPid ! {"Abort"};
+
+send_neighbor(ParentIndex, NodeCount, Sum, Weight, Topology, ParentPid, Master, SendCount) ->
+    NeighborList = get_neighbor_list(ParentIndex, Topology, NodeCount, []),
+    RandomNeighborIndex = rand:uniform(length(NeighborList)),
+    Neighbor = lists:nth(RandomNeighborIndex, NeighborList),
+    MasterAlive = is_process_alive(Master),
+    if(MasterAlive) ->
+        master ! {self(), Neighbor};
+    true ->
+        ok
+    end,
     receive
-        {Sum, Weight} ->
-            Newsum = Cursum + Sum,
-            Newweight = Curweight + Weight,
-            % Get all neighbor
-            % Select one neighbor
-            % Get Pid of the nieghbor
-            % Send message to the neighbor
-            % check for exit condition
-            cur_state(Newsum, Newweight, Index, NodeCount, Topology)
-            % change 
+        {NeighborPid, FinishCount} ->
+            NeighborAlive = is_process_alive(NeighborPid),
+            if (NeighborAlive) ->
+                % io:fwrite("~p sending rumor to ~p\n", [ParentIndex, Neighbor]),
+                NeighborPid ! {Sum/2, Weight/2};
+                % ParentPid ! {Sum/2, Weight/2, self()},
+                % NewSendCount = 0;
+            true ->
+                if (FinishCount == NodeCount) ->
+                    send_neighbor(ParentIndex, NodeCount, Sum, Weight, Topology, ParentPid, Master, NodeCount);
+                true ->
+                    % if (SendCount == 10) ->
+                    %     if (Algorithm == 4) ->
+                    %         Algorithm = 1,
+                    io:fwrite("~p trying to send ", [ParentIndex]),
+                    send_neighbor(ParentIndex, NodeCount, Sum, Weight, Topology, ParentPid, Master, SendCount)
+                end
+            end,
+    ParentPid
+    end.
+    % send_neighbor(ParentIndex, NodeCount, Sum/2, Weight/2, Topology, ParentPid, Master, NewSendCount).
 
+check_convergence(OldRation, NewRatio) ->
+    MinDiff = math:pow(10, -10),
+    Diff = NewRatio - OldRation,
+    if (Diff < 0) ->
+        ModDiff = -1.0*Diff;
+    true ->
+        ModDiff = Diff
+    end,
+    if (ModDiff < MinDiff) ->
+        true;
+    true ->
+        false
     end.
 
-start(Index, NodeCount, Topology) ->
-    io:fwrite(" Started Push-Sum Node with pid ~p\n", [self()]),
-    cur_state(Index, 1, Index, NodeCount, Topology).
+cur_state(_, _, Index, _, _, _, Master, 3) ->
+    % exit(SenderPid, ok),
+    Master ! {Index},
+    io:fwrite("~p shutting down\n", [Index]);
+
+
+
+cur_state(Cursum, Curweight, Index, NodeCount, Topology, SenderPid, Master, ConvergeCount) ->
+    receive
+        {Sum, Weight} ->
+            io:fwrite("Message is with ~p\n", [Index]),
+            Newsum = Cursum + Sum,
+            Newweight = Curweight + Weight,
+
+            % if (SenderPid == self()) ->
+            %     ok;
+            % true ->
+            %     % io:fwrite("~p Killing process \n", [Index]),
+            %     exit(SenderPid, ok)
+            % end,
+            NewSenderPid = spawn(push_sum_node, send_neighbor, [Index, NodeCount, Newsum, Newweight, Topology, self(), Master, 0]),
+            Converged = check_convergence(Cursum/Curweight, Newsum/Newweight),
+            if (Converged == true) ->
+                % io:fwrite("~p has converged count: ~p\n", [Index, ConvergeCount+1]),
+                NewConvergeCount = ConvergeCount+1;
+            true ->
+                % io:fwrite("~p converge count set to 0: \n", [Index]),
+                NewConvergeCount = 0
+            end,
+
+            
+            cur_state(Newsum/2, Newweight/2, Index, NodeCount, Topology, NewSenderPid, Master, NewConvergeCount);
+
+        {Halfsum, Halfweight, ChildPid} ->
+            % io:fwrite("~p Values halved \n", [Index]),
+            cur_state(Halfsum, Halfweight, Index, NodeCount, Topology, ChildPid, Master, ConvergeCount);
+        {AbortMessage} ->
+            io:fwrite("~p has no active neighbors, aborting with message ~p\n", [Index, AbortMessage]),
+            cur_state(Cursum, Curweight, Index, NodeCount, Topology, SenderPid, Master, 3)
+    end.
+
+start(Index, NodeCount, Topology, Master) ->
+    io:fwrite(" Started Push-Sum Node with Index ~p\n", [Index]),
+    cur_state(Index, 1, Index, NodeCount, Topology, self(), Master, 0).
